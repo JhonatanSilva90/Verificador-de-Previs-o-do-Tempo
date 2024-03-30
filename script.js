13a6529956818a92048923381b600aa29f090714
Jhonatan <jhonatan.dev90@gmail.com>
2024-03-30 19:23:10 -0300

    Criando as tags HTML por meio do JavaScript.

--- script.js
+++ script.js
@@ -1,3 +1,79 @@
+// Função para criar os elementos HTML dinâmicamente
+function criarElementos() {
+  // Criação dos elementos
+  let divMain = document.createElement("div");
+  divMain.id = "box-main";
+
+  let titulo = document.createElement("h1");
+  titulo.id = "titulo";
+  titulo.textContent = "Verificar Temperatura";
+
+  let inputCidade = document.createElement("input");
+  inputCidade.id = "input-cidade";
+  inputCidade.type = "text";
+  inputCidade.placeholder = "Digite a cidade";
+
+  let buttonBuscar = document.createElement("button");
+  buttonBuscar.id = "button-buscar";
+  buttonBuscar.onclick = pesquisar;
+
+  let imgBuscar = document.createElement("img");
+  imgBuscar.src = "https://www.svgrepo.com/show/488200/find.svg";
+  imgBuscar.alt = "lupa-buscar";
+  imgBuscar.id = "image-buscar";
+
+  let informacaoCidade = document.createElement("div");
+  informacaoCidade.id = "informacao-cidade";
+
+  let h2Cidade = document.createElement("h2");
+  h2Cidade.id = "cidade";
+
+  let pTemperatura = document.createElement("p");
+  pTemperatura.id = "temperatura";
+
+  let divInformacoesTempo = document.createElement("div");
+  divInformacoesTempo.id = "informacoes-tempo";
+
+  let pTempo = document.createElement("p");
+  pTempo.id = "tempo";
+
+  let pUmidade = document.createElement("p");
+  pUmidade.id = "umidade";
+
+  // Montagem da estrutura dos elementos
+  buttonBuscar.appendChild(imgBuscar);
+
+  divMain.appendChild(titulo);
+  divMain.appendChild(inputCidade);
+  divMain.appendChild(buttonBuscar);
+  divMain.appendChild(informacaoCidade);
+
+  informacaoCidade.appendChild(h2Cidade);
+  informacaoCidade.appendChild(pTemperatura);
+  informacaoCidade.appendChild(divInformacoesTempo);
+  informacaoCidade.appendChild(pUmidade);
+
+  divInformacoesTempo.appendChild(pTempo);
+
+  // Adicionando o elemento divMain ao corpo do documento
+  document.body.appendChild(divMain);
+  // ----------------------------
+  // Adicionando um ouvinte de evento para lidar com a tecla "Enter" pressionada no input
+  document
+    .getElementById("input-cidade")
+    .addEventListener("keypress", function (event) {
+      // Verifica se a tecla pressionada é "Enter"
+      if (event.key === "Enter") {
+        pesquisar();
+      }
+    });
+}
+
+// Chama a função para criar os elementos quando a página é carregada
+window.onload = function () {
+  criarElementos();
+};
+
 //Chave do servidor
 const key = "abaf8faaca83e783568de335ed462db3";
 
@@ -38,4 +114,7 @@ function pesquisar() {
   const cidade = document.getElementById("input-cidade").value;
 
   buscarCidade(cidade);
+
+  // Adiciona um ouvinte de evento para lidar com o clique no botão de busca
+  document.getElementById("button-buscar").addEventListener("click", pesquisar);
 }
